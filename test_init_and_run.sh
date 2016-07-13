7cd4dd6c500d4641a3a3f9a5fce8447add842b1b
Michaël Hauspie <michael.hauspie@univ-lille1.fr>
2016-07-13 13:42:23 +0200

    Adds a script to init DB if needed

---
+++ test_init_and_run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This script MUST be run in lup1 folder
+
+DB_HOST=db
+DB_USER=postgres
+DB_NAME=postgres
+SQL_FILES="01_init.sql 02_init.sql"
+SQL_FILES_PATH="db-scheme"
+SQL_QUERY="postgres -h $DB_HOST -U $DB_USER $DB_NAME"
+
+
+# Check if db already initialized
+$SQL_QUERY -A <<EOF | grep -q '0 rows'
+select * from CREDENTIAL;
+EOF
+
+
+if [ $? -eq 0 ] # Database must be populated
+then
+    for file in $SQL_FILES
+    do
+        $SQL_QUERY < "$SQL_FILES_PATH/$file"
+    done
+fi
+
+# Run application.
+mvn jetty:run
